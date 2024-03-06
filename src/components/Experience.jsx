4796dfba4faa1a17523aebd076a29a7bc1a0c439
Zelph Moreno <luiszelph.morenoruiz@gmail.com>
2024-03-06 17:55:20 -0600

    Se agrego .map al componente Experience para mostrar las imagenes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -35,39 +35,12 @@ const Experience = () => {
         
         <h2 className='font-semibold text-[40px]'>Experience</h2>
 
-        <div className='w-screen mt-[5%] pt-[10%] pb-[10%] bg-[#2B2B29] p-10'>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>    
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>    
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>    
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>    
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
-            <p className='text-white'>
-                Aqui va tener que ir algo de texto sobre mi personalidad</p>
+        <div className='w-screen grid-cols-3 grid mt-[5%] pt-[10%] pb-[10%] bg-[#2B2B29] p-10'>
+            {technologies.map(t => (
+                <div key={t.name} className='w-[100px] h-[100px]'>
+                    <img src={t.imageSrc} alt="technologie"/>
+                </div>
+            ))}
         </div>
 
     </div>
